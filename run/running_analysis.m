93443033d1969ce6311567e522f46af6bff8c904
waterzhaojun <zhaojunwater@gmail.com>
2020-11-21 14:56:56 -0500

    update

--- run/running_analysis.m
+++ run/running_analysis.m
@@ -2,8 +2,15 @@ function running_analysis(animal, date, run, varargin)
 parser = inputParser;
 addRequired(parser, 'animal', @ischar );
 addRequired(parser, 'date', @ischar);
-addRequired(parser, 'run', @ischar); 
+addRequired(parser, 'run', @isnumeric); 
 parse(parser, animal, date, run, varargin{:});
+
+root = sbxDir(animal, date, run);
+root = root.runs{1}.path;
+root = correct_folderpath([correct_folderpath(root), 'running']);
+if ~exist(root, 'dir')
+   mkdir(root)
+end
 
 path = sbxPath(animal, date, run, 'quad'); 
 inf = sbxInfo(path, true);
@@ -17,11 +24,13 @@ result = struct();
 result.array = getRunningArray(path);
 [tmp, result.secarray] = get_bout(result.array, scanrate);
 result.bout = tmp.bout;
+
+% plot the response pdf =============================================
 plot(result.secarray);
 hold on
 for i = 1:length(result.bout)
-    boutstart = result.bout{i}.startsec;
-    boutend = result.bout{i}.endsec;
+    boutstart = max(result.bout{i}.startsec-1, 1);
+    boutend = min(result.bout{i}.endsec+1, length(result.array));
     direction = result.bout{i}.direction;
     if direction == 1
         plot([boutstart:boutend], result.secarray(boutstart:boutend),'color','green')
@@ -30,5 +39,10 @@ for i = 1:length(result.bout)
     end
 end
 hold off
+saveas(gcf,[root, 'response.pdf'])
+close;
+
+% save result ========================================================
+save([root, 'result.mat'], 'result');
 
 end
