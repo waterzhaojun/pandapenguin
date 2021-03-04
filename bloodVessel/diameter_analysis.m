95ed2333043e93b3db220cfc347bc1728608cf5e
waterzhaojun <jzhao1@bidmc.harvard.edu>
2021-03-04 13:38:22 -0500

    update

--- bloodVessel/diameter_analysis.m
+++ bloodVessel/diameter_analysis.m
@@ -12,6 +12,7 @@ folder = correct_folderpath(folder);
 bvfilesys = bv_file_system();
 result = load([folder, bvfilesys.resultpath]);
 result = result.result;
+ref = imread([folder, result.refpath]);
 % Until to this step, the result should contain path information and roi
 % mask and angles. We start to build extra value.
 
@@ -19,41 +20,61 @@ result = result.result;
 [animal, date, run] = pathTranslate(folder);
 [result.x_ratio, result.y_ratio] = diameter_pixel_ratio(animal, date, run);
 
-subplotnum = 2*length(result.roi);
-figure();
+
+% figure('Position', [10 10 2500, 600 * 2 * length(result.roi)]);
+% tiledlayout(2*length(result.roi), 1);
+roi_per_page = 3;
+
 for i = 1:length(result.roi)
-   tmpposition = result.roi{i}.position;
-   tmpbw = result.roi{i}.BW;
-   if strcmp(tmpposition, 'vertical')
+    tmpposition = result.roi{i}.position;
+    tmpbw = result.roi{i}.BW;
+    if strcmp(tmpposition, 'vertical')
        [diameter, response_fig, response_mov] = vertical_diameter_measure(mx, tmpbw);
        result.roi{i}.diameter = diameter * (result.x_ratio + result.y_ratio)/2;
        result.roi{i}.response_mov_path= ['roi_', num2str(i),'_response_mov.tif'];
        % To avoid produce super big tif, I downsample output mov to 1hz.
        response_mov = downsamplef(response_mov, round(result.scanrate));
-      
+
        if max(response_mov, [],'all') > 256
           response_mov = response_mov/256;
        end
        mx2tif(uint8(response_mov), [folder,result.roi{i}.response_mov_path]);
-   elseif strcmp(result.roi{i}.position, 'horizontal')
+    elseif strcmp(result.roi{i}.position, 'horizontal')
        tmpangle = result.roi{i}.angle;
        [diameter, response_fig] = calculate_diameter(mx, tmpbw, tmpangle);
        result.roi{i}.diameter = diameter * ...
            sqrt(cos(abs(tmpangle)*pi/180)^2*result.x_ratio^2 + sin(abs(tmpangle)*pi/180)^2*result.y_ratio^2);
-           
-   end
-   
-    subplot(subplotnum, 1, 2*i-1);
+
+    end
+
+    if rem(i,roi_per_page) == 1
+        figure('Position', [10 10 1500, 600 * 2 * roi_per_page *1.5]);
+        tiledlayout(2*roi_per_page, 5);
+    end
+
+    nexttile([1,3])
     plot(result.roi{i}.diameter);
     xlim([1,length(result.roi{i}.diameter)]);
+    if isfield(result.roi{i}, 'id')
+        title(result.roi{i}.id);
+    end
 
-    subplot(subplotnum, 1, 2*i);
+    nexttile([2,2])
+    ref_with_this_mask = addroi(ref, result.roi{1}.BW);
+    imshow(imresize(ref_with_this_mask, [NaN, 600]));
+
+    nexttile([1,3])
     imshow(imresize(uint16(response_fig),[100,1500])); % Right now just use manual way to define ratio. Need to change to a better way.
 
+    if rem(i, roi_per_page) == 0 || i == length(result.roi)
+        tmpresponsefile = strsplit(result.response_fig_path, '.');
+        tmpresponsefile = [tmpresponsefile{1}, '_', num2str(ceil(i/roi_per_page)), '.', tmpresponsefile{2}];
+        saveas(gcf,[folder,tmpresponsefile]);
+        close;
+    end
+    
 end
 
-saveas(gcf,[folder,result.response_fig_path]);
-close;
 
 save([folder,bvfilesys.resultpath], 'result');
 
--- bloodVessel/diameter_prepare_mx.m
+++ bloodVessel/diameter_prepare_mx.m
@@ -8,6 +8,7 @@ addRequired(parser, 'run', @(x) isnumeric(x) && isscalar(x) && (x>0));
 addOptional(parser, 'pmt', 0, @(x) isnumeric(x) && isscalar(x) && (x>=0) && (x<2));
 addParameter(parser, 'layer', 'all');
 addParameter(parser, 'smooth', 0, @(x) isnumeric(x) && isscalar(x) && (x >= 0));
+addParameter(parser, 'output_mov', true, @islogical);
 addParameter(parser, 'output_mov_fs', 1, @(x) isnumeric(x) && isscalar(x) && (x >= 0)); % suppose output 1hz mov.
 
 %addParameter(parser, 'output_response_fig_width', 1000, @(x) isnumeric(x) && isscalar(x) && (x > 0)); % The output is not exactly 1000px, but close to 1000 based on the bint size.
@@ -16,6 +17,7 @@ parse(parser,animalID, dateID, run, varargin{:});
 pmt = parser.Results.pmt;
 layer = parser.Results.layer;
 smooth = parser.Results.smooth;
+output_mov = parser.Results.output_mov;
 output_mov_fs = parser.Results.output_mov_fs;
 
 % Prepare data matrix ================================================
@@ -63,23 +65,23 @@ end
 
 % Save mov sample. This sample is used only to check, not for future check.
 % The rate is 1hz.
+if output_mov
+    output_mov_fbint = round(check_scan_rate(inf) / check_scan_layers(inf) / output_mov_fs);
+    samplemov_f = floor(f/output_mov_fbint)*output_mov_fbint;
+    mxmov = mx(:,:,:,1:samplemov_f);
+    if output_mov_fbint > 1
+        disp(['bint by ', num2str(output_mov_fbint), ' the mx to output sample mov']);
 
-output_mov_fbint = round(check_scan_rate(inf) / check_scan_layers(inf) / output_mov_fs);
-samplemov_f = floor(f/output_mov_fbint)*output_mov_fbint;
-mxmov = mx(:,:,:,1:samplemov_f);
-if output_mov_fbint > 1
-    disp(['bint by ', num2str(output_mov_fbint), ' the mx to output sample mov']);
-    
-    
-    mxmov = reshape(mxmov, r,c,ch,output_mov_fbint,[]);
-    mxmov = squeeze(mean(mxmov, 4));
-    mxmov = reshape(mxmov, r,c,ch,[]);
-elseif output_mov_fbint < 1
-    disp(['Cannot bint by ', num2str(output_mov_fbint), '. Just save not bint sample mov.']);
+
+        mxmov = reshape(mxmov, r,c,ch,output_mov_fbint,[]);
+        mxmov = squeeze(mean(mxmov, 4));
+        mxmov = reshape(mxmov, r,c,ch,[]);
+    elseif output_mov_fbint < 1
+        disp(['Cannot bint by ', num2str(output_mov_fbint), '. Just save not bint sample mov.']);
+    end
+    mxmov = uint16(mxmov);
+    mx2tif(mxmov, [outputpath,'mov.tif']);
 end
-mxmov = uint16(mxmov);
-mx2tif(mxmov, [outputpath,'mov.tif']);
-
 
 
 
